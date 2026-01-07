14a2c8e9d2ecf8c1b39a23f3574038a12dcd4082
Teddy See <teddy@tsee.dev>
2026-01-07 22:39:18 +0000

    feat: add cobra rootcmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "sp",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+		os.Exit(0)
+	},
+}
 
 func main() {
-	fmt.Println("Hi")
+	cobra.CheckErr(rootCmd.Execute())
 }
