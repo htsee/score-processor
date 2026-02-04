a66360948e8b0798d3c99a056293470aa9761d3e
Teddy See <teddy@tsee.dev>
2026-02-04 17:26:48 +0000

    feat: add batch and singular functions to benchmark

--- internal/cmd/cmd_test.go
+++ internal/cmd/cmd_test.go
@@ -8,11 +8,11 @@ import (
 	"github.com/htsee/score-processor/internal/cmd"
 )
 
-var pdfDir = "../../testdata/pdf"
+const pdfDir = "../../testdata/pdf"
 
-var pageDir = "../../testdata/pages"
+const pageDir = "../../testdata/pages"
 
-var cutDir = "../../testdata/cut"
+const cutDir = "../../testdata/cut"
 
 func fileList(dir string) ([]string, error) {
 	files, err := os.ReadDir(dir)
@@ -31,44 +31,99 @@ var pdfList, _ = fileList(pdfDir)
 var pageList, _ = fileList(pageDir)
 var cutList, _ = fileList(cutDir)
 
-func BenchmarkConvert(b *testing.B) {
-	if err := cmd.ConvertBatch(pdfList, b.TempDir(), "1-N"); err != nil {
+var pdfFirst = pdfList[0]
+var pageFirst = pageList[0]
+
+const defaultPages = "1-N"
+
+func BenchmarkConvertBatch(b *testing.B) {
+	if err := cmd.ConvertBatch(pdfList, b.TempDir(), defaultPages); err != nil {
 		b.Error(err)
 	}
 }
 
-func BenchmarkCut(b *testing.B) {
+func BenchmarkConvertSingle(b *testing.B) {
+	if err := cmd.Convert(pdfFirst, b.TempDir(), defaultPages); err != nil {
+		b.Error(err)
+	}
+}
+
+func BenchmarkCutBatch(b *testing.B) {
 	if err := cmd.CutBatch(pageList, b.TempDir()); err != nil {
 		b.Error(err)
 	}
 }
 
-func BenchmarkDenoise(b *testing.B) {
-	if err := cmd.DenoiseBatch(pageList, b.TempDir(), 2); err != nil {
+func BenchmarkCutSingle(b *testing.B) {
+	if err := cmd.Cut(pageFirst, b.TempDir()); err != nil {
 		b.Error(err)
 	}
 }
 
-func BenchmarkDeskew(b *testing.B) {
+const defaultSize = 2
+
+func BenchmarkDenoiseBatch(b *testing.B) {
+	if err := cmd.DenoiseBatch(pageList, b.TempDir(), defaultSize); err != nil {
+		b.Error(err)
+	}
+}
+
+func BenchmarkDenoiseSingle(b *testing.B) {
+	if err := cmd.DenoiseCmdExecute(pageFirst, b.TempDir(), defaultSize); err != nil {
+		b.Error(err)
+	}
+}
+
+func BenchmarkDeskewBatch(b *testing.B) {
 	if err := cmd.DeskewBatch(pageList, b.TempDir()); err != nil {
 		b.Error(err)
 	}
 }
 
-func BenchmarkFit(b *testing.B) {
-	if err := cmd.FitBatch(pageList, b.TempDir(), 16.0/9.0); err != nil {
+func BenchmarkDeskewSingle(b *testing.B) {
+	if err := cmd.DeskewCmdExecute(pageFirst, b.TempDir()); err != nil {
 		b.Error(err)
 	}
 }
 
-func BenchmarkPad(b *testing.B) {
-	if err := cmd.PadBatch(pageList, b.TempDir(), 10, 10); err != nil {
+const defaultRatio = 16.0 / 9.0
+
+func BenchmarkFitBatch(b *testing.B) {
+	if err := cmd.FitBatch(pageList, b.TempDir(), defaultRatio); err != nil {
 		b.Error(err)
 	}
 }
 
-func BenchmarkRotate(b *testing.B) {
-	if err := cmd.RotateBatch(pageList, b.TempDir(), 90); err != nil {
+func BenchmarkFitSingle(b *testing.B) {
+	if err := cmd.FitCmdExecute(pageFirst, b.TempDir(), defaultRatio); err != nil {
+		b.Error(err)
+	}
+}
+
+const defaultVpad, defaultHpad = 10, 10
+
+func BenchmarkPadBatch(b *testing.B) {
+	if err := cmd.PadBatch(pageList, b.TempDir(), defaultVpad, defaultHpad); err != nil {
+		b.Error(err)
+	}
+}
+
+func BenchmarkPadSingle(b *testing.B) {
+	if err := cmd.PadCmdExecute(pageFirst, b.TempDir(), defaultVpad, defaultHpad); err != nil {
+		b.Error(err)
+	}
+}
+
+const defaultAngle = 90
+
+func BenchmarkRotateBatch(b *testing.B) {
+	if err := cmd.RotateBatch(pageList, b.TempDir(), defaultAngle); err != nil {
+		b.Error(err)
+	}
+}
+
+func BenchmarkRotateSingle(b *testing.B) {
+	if err := cmd.RotateCmdExecute(pageFirst, b.TempDir(), defaultAngle); err != nil {
 		b.Error(err)
 	}
 }
@@ -79,8 +134,16 @@ func BenchmarkSplice(b *testing.B) {
 	}
 }
 
-func BenchmarkTrim(b *testing.B) {
-	if err := cmd.TrimBatch(pageList, b.TempDir(), 5, 5, 0, 0); err != nil {
+const defaultTop, defaultBottom, defaultLeft, defaultRight = 5, 5, 5, 5
+
+func BenchmarkTrimBatch(b *testing.B) {
+	if err := cmd.TrimBatch(pageList, b.TempDir(), defaultTop, defaultBottom, defaultLeft, defaultRight); err != nil {
+		b.Error(err)
+	}
+}
+
+func BenchmarkTrimSingle(b *testing.B) {
+	if err := cmd.TrimCmdExecute(pageFirst, b.TempDir(), defaultTop, defaultBottom, defaultLeft, defaultRight); err != nil {
 		b.Error(err)
 	}
 }
