a397f602008413cc74784c7c4a4cbc0386252f98
Teddy See <teddy@tsee.dev>
2026-02-08 17:45:15 +0000

    feat: concurrency for vsplice

--- internal/cmd/vsplice.go
+++ internal/cmd/vsplice.go
@@ -6,6 +6,7 @@ import (
 	"github.com/htsee/score-processor/internal/util"
 	"github.com/spf13/cobra"
 	"gocv.io/x/gocv"
+	"golang.org/x/sync/errgroup"
 )
 
 var VSpliceCmd = &cobra.Command{
@@ -29,6 +30,7 @@ func VSplice(inputs []string, destination string) error {
 	maxHeight := 0
 	var pages []gocv.Mat
 	index := 1
+	var g errgroup.Group
 	for i, input := range inputs {
 		page := gocv.IMRead(input, gocv.IMReadGrayScale)
 		if page.Empty() {
@@ -41,13 +43,14 @@ func VSplice(inputs []string, destination string) error {
 
 		pages = append(pages, page)
 		if len(pages) >= 2 || i == len(inputs)-1 {
-			if err := util.Combine(pages, maxHeight, index, "vertical", destination); err != nil {
-				return err
-			}
+			pagesCopy := pages
+			g.Go(func() error {
+				return util.Combine(pagesCopy, maxHeight, index, "vertical", destination)
+			})
 			index++
 			maxHeight = 0
 			pages = pages[:0]
 		}
 	}
-	return nil
+	return g.Wait()
 }
